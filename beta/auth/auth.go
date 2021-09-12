220e927b2bc79574144455fca1222c55cc9f50f0
André Eriksson <andre@encore.dev>
2021-09-12 16:28:15 +0200

    beta/auth: add WithContext
    See encoredev/encore#85

--- beta/auth/auth.go
+++ beta/auth/auth.go
@@ -1,5 +1,7 @@
 package auth
 
+import "context"
+
 // UID is a unique identifier representing a user (a user id).
 type UID string
 
@@ -20,5 +22,21 @@ func UserID() (UID, bool) {
 //   if !ok { /* ... */ }
 //
 func Data() interface{} {
+	panic("encore apps must be run using the encore command")
+}
+
+// WithContext returns a new context that sets the auth information for outgoing API calls.
+// It does not affect the auth information for the current request.
+//
+// Passing in an empty string as the uid results in unsetting the auth information,
+// causing future API calls to behave as if there was no authenticated user.
+//
+// If the application's auth handler returns custom auth data, two additional
+// requirements exist. First, the data parameter passed to WithContext must be of
+// the same type as the auth handler returns. Second, if the uid argument is not
+// the empty string then data may not be nil. If these requirements are not met,
+// API calls made with these options will not be made and will immediately return
+// a client-side error.
+func WithContext(ctx context.Context, uid UID, data interface{}) context.Context {
 	panic("encore apps must be run using the encore command")
 }
